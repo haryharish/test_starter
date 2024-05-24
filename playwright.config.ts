ee88e2ec0f92d3ac680453f27940e61980abecf5
haryharish <harryharish55@gmail.com>
2024-05-24 17:10:02 +0530

    update billing information

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,7 +44,7 @@ const config: PlaywrightTestConfig = {
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    //screenshot: "on",
+    screenshot: "on",
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
 
