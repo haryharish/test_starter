1896cbcdf3016e39dbddb639d490c3f6678b7f02
Harish <harryharish55@gmail.com>
2024-05-24 20:35:59 +0530

    search order

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,19 +28,19 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-   //reporter: 'html',
-  reporter: [
-    ["dot"],
-    ["line"],
-    ["html"],
-    [
-      "@estruyf/github-actions-reporter",
-      {
-        useDetails: true,
-        showError: true,
-      },
-    ],
-  ],
+   reporter: 'html',
+  //reporter: [
+    //["dot"],
+    //["line"],
+    //["html"],
+    //[
+      //"@estruyf/github-actions-reporter",
+      //{
+        //useDetails: true,
+        //showError: true,
+      //},
+    //],
+  //],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -53,7 +53,7 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     //trace : 'off',
-    //headless : false,
+    headless : false,
 
   },
 
---
+++ tests/searchorder.spec.js
@@ -0,0 +1,17 @@
+import {test, expect} from '@playwright/test'
+test ('searchorder', async({page})=>{
+
+    await page.goto("https://minimals.cc/.");
+    await page.locator("//a[normalize-space()='Login']").click();
+    await page.locator("(//input[@id=':r28:'])[1]").fill("demo@minimals.cc");
+    await page.locator("(//input[@id=':r29:'])[1]").fill("demo1234");
+    await page.locator("(//button[normalize-space()='Login'])[1]").click();
+
+    await page.locator("//span[contains(text(),'order')]").click();
+    await page.locator("//span[contains(text(),'list')]").click();
+    await page.getByPlaceholder('Search customer or order number...').fill("cor");
+
+    console.log = (await page.getByText("Cortez Herring").textContent());
+    await expect (page.getByText("Cortez Herring")).toContainText("Cortez Herring");
+
+})
